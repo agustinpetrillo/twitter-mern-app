225bdcfc38ef7b2ac4a5f7779b0e1aec8c2ea8a4
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-10-03 15:39:15 -0300

    removed imports from route.ts in nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,4 @@
-import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/libs/prismadb";
 import { authOptions } from "@/utils/authOptions";
 
 const handler = NextAuth(authOptions);
