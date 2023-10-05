9482845d27c0316db066746dcaf21af7ed0c695c
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-10-04 22:57:03 -0300

    new error response

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -30,8 +30,7 @@ export async function POST(req: Request) {
       );
 
     return NextResponse.json({ user }, { status: 201 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error }, { status: 500 });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
