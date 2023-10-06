ca0932742ff8fc301445c910dbaeeeaac224f392
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-10-06 14:17:13 -0300

    handle click to user profile

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -6,8 +6,6 @@ export default function Avatar({ userId }: AvatarProps) {
   const { data } = useCurrentUser(userId);
   const router = useRouter();
 
-  console.log(data);
-
   const goToClickedProfile = async (e: any) => {
     e.stopPropagation();
 
@@ -15,15 +13,13 @@ export default function Avatar({ userId }: AvatarProps) {
   };
 
   return (
-    <div className="">
-      <Image
-        src={data || "/images/profile-without-image.png"}
-        alt={data}
-        height={50}
-        width={50}
-        className="object-cover transition-all rounded-full cursor-pointer hover:opacity-90"
-        onClick={(e) => goToClickedProfile(e)}
-      />
-    </div>
+    <Image
+      src={data || "/images/profile-without-image.png"}
+      alt={data}
+      height={50}
+      width={50}
+      className="object-cover transition-all rounded-full cursor-pointer hover:opacity-90"
+      onClick={(e) => goToClickedProfile(e)}
+    />
   );
 }
