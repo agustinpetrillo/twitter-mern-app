43b8392b460c281d363878607b1da66cf987c45f
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-10-06 14:53:10 -0300

    creating user profile

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,7 +1,35 @@
-export default function User() {
+"use client";
+
+import Image from "next/image";
+import Header from "@/components/Header";
+import Loader from "@/components/reutilizable/Loader";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+
+export default function User({
+  params: { username },
+}: {
+  params: { username: string };
+}) {
+  const { data, isLoading } = useCurrentUser(username);
+
+  if (isLoading) return <Loader />;
+
   return (
     <div>
-      <div>page</div>
+      <Header name="User profile" />
+      <div>
+        {/* <div className="min-h-0 py-2 bg-neutral-700">
+          {data.coverImage && (
+            <Image
+              src={data.coverImage || "/images/profile-without-image.png"}
+              alt={data.name}
+              height={50}
+              width={50}
+              className="rounded-full"
+            />
+          )}
+        </div> */}
+      </div>
     </div>
   );
 }
--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -1,9 +1,9 @@
 import { fetcher } from "@/libs/fetcher";
 import useSWR from "swr";
 
-export const useCurrentUser = (userId: string) => {
+export const useCurrentUser = (username: string) => {
   const { data, isLoading, error, mutate } = useSWR(
-    `/api/users/${userId}`,
+    `/api/users/${username}`,
     fetcher
   );
 
