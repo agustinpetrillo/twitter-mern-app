97def998c9f479f1f9fd7287fe81dd4c5c99f4ef
Agustin Petrillo <agustinpetrillo1@gmail.com>
2023-10-18 13:58:17 -0300

    tweets post

--- src/app/api/tweets/route.ts
+++ src/app/api/tweets/route.ts
@@ -15,11 +15,11 @@ export async function GET(req: Request, res: Response) {
 }
 
 export async function POST(req: Request, res: Response) {
-  const { body, userId } = await req.json();
+  const { body, userId, likesIds } = await req.json();
 
   try {
     if (!body || !userId)
-      return Response.json({ error: "Invalid credentials" }, { status: 422 });
+      return Response.json({ error: "Invalid data" }, { status: 422 });
 
     await connectToDb();
 
@@ -29,10 +29,7 @@ export async function POST(req: Request, res: Response) {
       return NextResponse.json({ message: "Invalid user" }, { status: 401 });
 
     const tweet = await prisma.post.create({
-      data: {
-        body,
-        userId,
-      },
+      data: { body, userId, likesIds },
     });
 
     return NextResponse.json({ tweet }, { status: 201 });
